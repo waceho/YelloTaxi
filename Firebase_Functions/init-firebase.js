d0b47db413c3e19b91039989b24984258345ca92
waceho <amancio.perros@gmail.com>
2019-04-21 15:58:15 +0200

    save
    save

--- Firebase_Functions/init-firebase.js
+++ Firebase_Functions/init-firebase.js
@@ -1,6 +1,6 @@
 // TODO: Replace the following with your app's Firebase project configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBH2Qx7KquoyN6eBUjSRKtiDkdPfaRtuXU",
+    apiKey: "API_",
     authDomain: "benintaxi-8efbe.firebaseapp.com",
     databaseURL: "https://benintaxi-8efbe.firebaseio.com",
     projectId: "benintaxi-8efbe",
