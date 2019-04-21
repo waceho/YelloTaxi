7c38970a2b664f0a0ff816adfe409199f7aa734f
waceho <amancio.perros@gmail.com>
2019-04-21 15:06:38 +0200

    save config

---
+++ Firebase_Functions/init-firebase.js
@@ -0,0 +1,12 @@
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBH2Qx7KquoyN6eBUjSRKtiDkdPfaRtuXU",
+    authDomain: "benintaxi-8efbe.firebaseapp.com",
+    databaseURL: "https://benintaxi-8efbe.firebaseio.com",
+    projectId: "benintaxi-8efbe",
+    storageBucket: "benintaxi-8efbe.appspot.com",
+    messagingSenderId: "983833722602"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
